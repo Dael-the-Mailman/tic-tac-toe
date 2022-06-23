bf8ef759e9fb2ae65daea6b0cb00b0458bcfed76
Kaleb Ugalde <kalebugalde@gmail.com>
2022-06-22 22:37:05 -0700

    Created first implementation of making moves in the game

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from copy import deepcopy
 
 class Board():
     def __init__(self, board=None):
-        self.player_x = 'x'
-        self.player_o = 'o'
+        self.player_1 = 'x'
+        self.player_2 = 'o'
         self.empty_square = '.'
 
         self.position = {}
@@ -23,6 +23,14 @@ class Board():
             for col in range(3):
                 self.position[row, col] = self.empty_square
 
+    def make_move(self, row, col):
+        board = Board()
+        board.position[row, col] = self.player_1
+
+        (board.player_1, board.player_2) = (board.player_2, board.player_1) 
+
+        return board
+
     def __str__(self):
         board_string = ''
 
@@ -31,8 +39,21 @@ class Board():
                 board_string += ' %s' % self.position[row, col]
 
             board_string += "\n"
+        
+        if self.player_1 == 'x':
+            board_string = '\n-------------\n"x" to move:\n-------------\n\n' + board_string
+        elif self.player_1 == 'o':
+            board_string = '\n-------------\n"o" to move:\n-------------\n\n' + board_string
+
         return board_string
 
 if __name__ == '__main__':
     board = Board()
+
     print(board)
+
+    board = board.make_move(0,0)
+
+    board_1 = Board(board)
+
+    print(board_1)
