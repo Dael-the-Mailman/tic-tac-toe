6bb414673714830fe34aac40bb83d4771f98f546
Kaleb Ugalde <kalebugalde@gmail.com>
2022-06-26 22:22:06 -0700

    Refactor cpp file second time to match python implementation

--- main.cpp
+++ main.cpp
@@ -2,6 +2,57 @@
 
 using namespace std;
 
+struct Move{
+    int row;
+    int col;
+};
+
+char check(vector<vector<char> > gs){
+    // Check if player wins
+    if (gs[0][0] != '-')
+    {
+        bool vert = (gs[0][0] == gs[1][0]) && (gs[0][0] == gs[2][0]);
+        bool hort = (gs[0][0] == gs[0][1]) && (gs[0][0] == gs[0][2]);
+        if (vert || hort)
+        {
+            return gs[0][0];
+        }
+    }
+    if (gs[1][1] != '-')
+    {
+        bool diag1 = (gs[1][1] == gs[0][0]) && (gs[1][1] == gs[2][2]);
+        bool diag2 = (gs[1][1] == gs[0][2]) && (gs[1][1] == gs[2][0]);
+        bool vert = (gs[1][1] == gs[0][1]) && (gs[1][1] == gs[2][1]);
+        bool hort = (gs[1][1] == gs[1][0]) && (gs[1][1] == gs[1][2]);
+        if (diag1 || diag2 || vert || hort)
+        {
+            return gs[1][1];
+        }
+    }
+    if (gs[2][2] != '-')
+    {
+        bool vert = (gs[2][2] == gs[0][2]) && (gs[2][2] == gs[1][2]);
+        bool hort = (gs[2][2] == gs[2][1]) && (gs[2][2] == gs[2][0]);
+        if (vert || hort)
+        {
+            return gs[2][2];
+        }
+    }
+    // Check if board's filled
+    for (int row = 0; row < 3; row++)
+    {
+        for (int col = 0; col < 3; col++)
+        {
+            if (gs[row][col] == '-')
+            {
+                return '-';
+            };
+        }
+    }
+    
+    return 'd';
+}
+
 class GameState
 {
 public:
@@ -20,51 +71,21 @@ public:
 
     bool isDone()
     {
-        // Check if player wins
-        if (board[0][0] != '-')
-        {
-            bool vert = (board[0][0] == board[1][0]) && (board[0][0] == board[2][0]);
-            bool hort = (board[0][0] == board[0][1]) && (board[0][0] == board[0][2]);
-            if (vert || hort)
-            {
-                cout << "Player " << board[0][0] << " Wins!\n";
-                return true;
-            }
+        char result = check(board);
+        if(result == '-')
+            return false;
+        
+        switch(result){
+            case 'x':
+                cout << "Player X Wins!\n";
+                break;
+            case 'o':
+                cout << "Player O Wins!\n";
+                break;
+            case 'd':
+                cout << "Draw!\n";
+                break;
         }
-        if (board[1][1] != '-')
-        {
-            bool diag1 = (board[1][1] == board[0][0]) && (board[1][1] == board[2][2]);
-            bool diag2 = (board[1][1] == board[0][2]) && (board[1][1] == board[2][0]);
-            bool vert = (board[1][1] == board[0][1]) && (board[1][1] == board[2][1]);
-            bool hort = (board[1][1] == board[1][0]) && (board[1][1] == board[1][2]);
-            if (diag1 || diag2 || vert || hort)
-            {
-                cout << "Player " << board[1][1] << " Wins!\n";
-                return true;
-            }
-        }
-        if (board[2][2] != '-')
-        {
-            bool vert = (board[2][2] == board[0][2]) && (board[2][2] == board[1][2]);
-            bool hort = (board[2][2] == board[2][1]) && (board[2][2] == board[2][0]);
-            if (vert || hort)
-            {
-                cout << "Player " << board[2][2] << " Wins!\n";
-                return true;
-            }
-        }
-        // Check if board's filled
-        for (int row = 0; row < 3; row++)
-        {
-            for (int col = 0; col < 3; col++)
-            {
-                if (board[row][col] == '-')
-                {
-                    return false;
-                };
-            }
-        }
-        cout << "Draw!\n";
         return true;
     }
 
@@ -131,87 +152,174 @@ public:
         return xToMove;
     }
 
-    vector<vector<char>> getBoard()
+    vector<vector<char> > getBoard()
     {
         return board;
     }
 
 private:
     // const int dim = 3;
-    vector<vector<char>> board;
+    vector<vector<char> > board;
     bool xToMove;
 };
 
-class Minimax
-{
+class MiniMax{
 public:
-    Minimax();
+    MiniMax(){
+        iterations = 0;
+    }
+
+    Move getBestMove(vector<vector<char> > &board, bool isMaximizing){
+        iterations = 0;
+        int bestScore = (isMaximizing) ? numeric_limits<int>::min() : numeric_limits<int>::max();
+        Move optimal;
+        int alpha = numeric_limits<int>::min();
+        int beta = numeric_limits<int>::max();
+        for(int row = 0; row < 3; row++){
+            for(int col = 0; col < 3; col++){
+                if(board[row][col] == '-'){
+                    if(isMaximizing){
+                        board[row][col] = 'x';
+                    } else {
+                        board[row][col] = 'o';
+                    }
+                    int score = evaluate(board, alpha, beta, !isMaximizing);
+                    board[row][col] = '-';
+                    if(isMaximizing){
+                        if(score > bestScore){
+                            bestScore = score;
+                            optimal = {row + 1, col + 1};
+                        }
+                    } else {
+                        if(score < bestScore){
+                            bestScore = score;
+                            optimal = {row + 1, col + 1};
+                        }
+                    }
+                }
+            }
+        }
+        return optimal;
+    }
+
+    int getIterations(){
+        return iterations;
+    }
 
 private:
+    int evaluate(vector<vector<char> > &board, int alpha, int beta, bool isMaximizing){
+        if(check(board) == 'x'){
+            return 1;
+        } else if (check(board) == 'o'){
+            return -1;
+        } else if (check(board) == 'd'){
+            return 0;
+        }
+
+        iterations++;
+        int bestScore = (isMaximizing) ? numeric_limits<int>::min() : numeric_limits<int>::max();
+        
+        for(int row = 0; row < 3; row++){
+            for(int col = 0; col < 3; col++){
+                if(board[row][col] == '-'){
+                    if(isMaximizing){
+                        board[row][col] = 'x';
+                    } else {
+                        board[row][col] = 'o';
+                    }
+                    int score = evaluate(board, alpha, beta, !isMaximizing);
+                    board[row][col] = '-';
+                    if(isMaximizing){
+                        bestScore = max(bestScore, score);
+                        if (score >= beta)
+                            return bestScore;
+                        if (score > alpha)
+                            alpha = score;
+                    } else {
+                        bestScore = min(bestScore, score);
+                        if (score <= alpha)
+                            return bestScore;
+                        if (score < beta)
+                            beta = score;
+                    }
+                }
+            }
+        }
+        
+        return bestScore;
+    }
+    int iterations;
 };
 
-class MCTS
-{
+class MonteCarlo{
 public:
-    MCTS();
+    MonteCarlo(){
 
+    }
+
+    Move getBestMove(){
+        return {-1,-1};
+    }
 private:
+    int evaluate(){
+        return 1;
+    }
 };
 
 int main()
 {
-    enum class Mode
-    {
-        pvp,
-        pvai,
-        aivai
-    };
-
-    enum class AI
-    {
-        mini,
-        mcts
-    };
-
     GameState gs;
-    Mode mode;
     int choice;
+    MiniMax mm;
+    MonteCarlo mc;
 
-    cout
-        << "Choose which mode to play: \n"
-        << "   0 - Player vs Player\n"
-        << "   1 - Player vs AI\n"
-        << "   2 - AI vs AI\n";
-    cin >> choice;
-    mode = (Mode)choice;
     while (!gs.isDone())
     {
-        switch (mode)
-        {
-        case Mode::pvp:
-            if (gs.isXTurn())
-            {
-                cout << "X to ";
-            }
-            else
-            {
-                cout << "O to ";
-            }
+        // if (gs.isXTurn())
+        // {
+        //     cout << "X to ";
+        // }
+        // else
+        // {
+        //     cout << "O to ";
+        // }
 
-            cout << "Move (Format: row col)\n";
+        // cout << "Move (Format: row col)\n";
+        // int r, c;
+        // cin >> r >> c;
+        // gs.move(r, c);
+        // gs.display();
+        // cout << "\n";
+
+        if (gs.isXTurn())
+        {
+            cout << "X to Move\n";
+            vector<vector<char> > board = gs.getBoard();
+            bool turn = gs.isXTurn();
+            Move m = mm.getBestMove(board, turn);
+            gs.move(m.row, m.col);
+            cout << mm.getIterations() << " iterations checked\n";
+        }
+        else
+        {
+            cout << "O to Move (Format: row col)\n";
             int r, c;
             cin >> r >> c;
             gs.move(r, c);
-            gs.display();
-            cout << "\n";
-            break;
-        case Mode::pvai:
-            cout << "Dum Dum\n";
-            break;
-        case Mode::aivai:
-            cout << "He he\n";
-            break;
         }
+
+        // if(gs.isXTurn()){
+        //     cout << "X to Move\n";
+        // } else {
+        //     cout << "O to Move\n";
+        // }
+
+        // vector<vector<char> > board = gs.getBoard();
+        // bool turn = gs.isXTurn();
+        // Move m = bestMove(board, turn);
+        // gs.move(m.row, m.col);
+
+        gs.display();
     }
     return 0;
 }
