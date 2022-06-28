6b00b34b114d75bd542eb09d612a04df586f110a
Kaleb Ugalde <kalebugalde@gmail.com>
2022-06-27 23:08:52 -0700

    Created print function and declared variables

--- main.cpp
+++ main.cpp
@@ -4,20 +4,27 @@ using Vec2D = std::vector<std::vector<char>>;
 using Vec1D = std::vector<char>;
 using Move = std::pair<int,int>;
 
-class TreeNode{
+// class TreeNode{
 
-};
+// };
 
-class MCTS{
+// class MCTS{
 
-};
+// };
 
 class Board{
-    Board(){
+public:
+    char player_1 = 'x';
+    char player_2 = 'o';
+    char empty_square = '.';
 
+    Vec2D position;
+
+    Board(){
+        this->position = Vec2D(3, Vec1D(3, '.'));
     }
 
-    Board(Vec2D board){
+    Board(const Board& board){
 
     }
 
@@ -41,16 +48,32 @@ class Board{
 
     }
 
-    std::ostream& operator<< (std::ostream &out, Board const& data){
+    void printBoard(){
+        std::string out = "";
+        if(this->player_1 == 'x'){
+            std::cout << "\n-------------\n\"x\" to move:\n-------------\n\n";
+        } else if(this->player_2 == 'o'){
+            std::cout << "\n-------------\n\"x\" to move:\n-------------\n\n";
+        }
 
+        for (int row = 0; row < 3; row++){
+            for (int col = 0; col < 3; col++){
+                std::cout << " " << this->position[row][col];
+            }
+            std::cout << "\n";
+        }
     }
-private:
-    char player_1 = 'x';
-    char player_2 = 'o';
-    char empty_square = '.';
 };
 
 int main(){
+    Board board;
 
+    board.printBoard();
+
+    board.position[1][1] = 'x';
+
+    Board board1(board);
+
+    board1.printBoard();
     return 0;
 }
