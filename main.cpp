38065e8a36f8f300ee0b9b837856239cf87a54b8
Kaleb Ugalde <kalebugalde@gmail.com>
2022-06-28 23:54:57 -0700

    Implemented isWin and generateStates methods

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ public:
     Vec2D position;
 
     Board(){
-        this->position = Vec2D(3, Vec1D(3, '.'));
+        this->position = Vec2D(3, Vec1D(3, this->empty_square));
     }
 
     Board(const Board& board):
@@ -54,11 +54,42 @@ public:
     }
 
     bool isWin(){
-        return false;
+        for(int i = 0; i < 3; i++){
+            if ((this->position[0][i] == this->player_2) &&
+                (this->position[1][i] == this->player_2) &&
+                (this->position[2][i] == this->player_2)){
+                return true;
+            }
+
+            if ((this->position[i][0] == this->player_2) &&
+                (this->position[i][1] == this->player_2) &&
+                (this->position[i][2] == this->player_2)){
+                return true;
+            }
+        }
+
+        bool diag_1 = (this->position[0][0] == this->player_2) &&
+                      (this->position[1][1] == this->player_2) &&
+                      (this->position[2][2] == this->player_2);
+
+        bool diag_2 = (this->position[2][0] == this->player_2) &&
+                      (this->position[1][1] == this->player_2) &&
+                      (this->position[0][2] == this->player_2);
+
+        return diag_1 || diag_2;
     }
 
-    std::vector<Move> generateStates(){
-        return std::vector<Move>();
+    std::vector<Board> generateStates(){
+        std::vector<Board> actions;
+        for(int row = 0; row < 3; row++){
+            for(int col = 0; col < 3; col++){
+                if(this->position[row][col] == this->empty_square){
+                    actions.push_back(this->makeMove(row, col));
+                }
+            }
+        }
+
+        return actions;
     }
 
     void gameLoop(){
@@ -85,19 +116,16 @@ public:
 int main(){
     Board board;
 
-    board.position[0][0] = 'o';
-    board.position[0][1] = 'x';
-    board.position[0][2] = 'x';
-    board.position[1][0] = 'o';
-    board.position[1][1] = 'x';
-    board.position[1][2] = 'x';
-    board.position[2][0] = 'o';
-    board.position[2][1] = 'x';
-    board.position[2][2] = 'x';
+    board = board.makeMove(1,1);
 
     board.printBoard();
 
+    auto actions = board.generateStates();
 
-    std::cout << board.isDraw() << "\n";
+    for(auto action : actions){
+        action.printBoard();
+    }
+
+    std::cout << board.player_2 << "\t" << board.isWin() << "\n";
     return 0;
 }
